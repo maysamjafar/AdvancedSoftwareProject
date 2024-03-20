9d154ad3be7915d916abc62d3fef14f15c96c0a2
maysamjafar <maysam.jafar2001@gmail.com>
2024-03-20 21:16:33 +0200

    On branch MaysamJafar

--- advanceSoftware/routes/Ap.js
+++ advanceSoftware/routes/Ap.js
@@ -1,15 +1,16 @@
 const express = require('express');
-const loginRouter = require('./login');
+const loginRouter = require ('./login');
 const signupRouter = require('./signup');
-
+const resourcesRouter = require('./APIs/Resources');
 const app = express();
 const port = 5000;
 
 // Use the login router for requests to /login
 app.use('/login', loginRouter);
+app.use('/API/Resources', resourcesRouter);
 
 // Use the signup router for requests to /signup
 app.use('/signup', signupRouter);
 app.listen(port, () => {
-  console.log('Server running on port ${port}');
+  console.log(`Server running on port ${port}`);
 });
