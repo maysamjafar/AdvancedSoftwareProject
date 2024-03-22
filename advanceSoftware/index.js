57208e9351283bbc577c77d060a27f450cced353
serrinaqqad <140744938+serrinaqqad@users.noreply.github.com>
2024-03-22 14:03:21 +0200

    secound commit

--- advanceSoftware/index.js
+++ advanceSoftware/index.js
@@ -4,11 +4,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const projectRouter = require('./routes/craftProjects.js');
 const finishProjectRouter = require('./routes/finishedProjects.js');
+const whatsappclient = require("./services/WhatsappClient.js")
+const messageRouter = require("./routes/messageRouter.js")
+
+whatsappclient.initialize()
+
 const app = express();
 const PORT = 3005;
 app.use(bodyParser.json());
 app.use("/projects", projectRouter);
 app.use("/finish-projects", finishProjectRouter);
+app.use("/massage", messageRouter);
 app.get("/", (req, res) => {
   console.log("test");
   res.send("Hello from home page");
